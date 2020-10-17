62cf94af65d05889d6c2e8e80fba4159981b906c
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-17 08:24:30 +0000

    [Add] controllerに、before_action :authenticate_user! を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def show
     @book = Book.find(params[:id])
   end
@@ -32,7 +33,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destoy
     redirect_to books_path
@@ -41,7 +42,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
