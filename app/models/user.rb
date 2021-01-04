876e29bce9d3ec277b0e7a78399d5d87e18b6f1f
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-04 06:44:20 +0000

    [Fix]has_manyが定義されていないエラーを解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
   has_many :entries, dependent: :destroy
 
 # 通知機能
-　has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
   def create_notification_follow!(current_user)
