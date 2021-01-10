50850fd09eb81b104f5e80231e8da179cdada859
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-10 07:47:52 +0000

    [Add]Topページにランキング機能を実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @books = Book.order("RANDOM()").limit(4)
+    @ranks = Book.find(Favorite.group(:book_id).order('count(book_id) desc').limit(4).pluck(:book_id))
   end
 
   def about
