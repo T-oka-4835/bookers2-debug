baf368590bedc547b97a2cffbc82a2d093232951
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-11 11:01:06 +0000

    [Fix]通知機能の修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,8 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
-    end 
-  end 
+    @notifications = current_user.passive_notifications.where(checked: false)
+      @notifications.each do |notification|
+        notification.update_attributes(checked: true)
+    end
+  end
 end
