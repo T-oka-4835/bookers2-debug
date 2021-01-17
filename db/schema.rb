3d0877719a50394b8c8fc6bdbe40cf11f1da56e5
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-17 05:01:34 +0000

    [Add]Bookモデルに、画像投稿用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_042252) do
+ActiveRecord::Schema.define(version: 2021_01_17_045959) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_03_042252) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
   create_table "entries", force: :cascade do |t|
