120dbb51ca8f8b1e690803fb243cd6875cc57fa3
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-24 08:46:49 +0000

    [Fix]カレンダーがviewに出るようになった

--- app/controllers/calenders_controller.rb
+++ app/controllers/calenders_controller.rb
@@ -1,4 +1,45 @@
 class CalendersController < ApplicationController
+  
   def index
+    @calender = Calender.all
   end
+  
+  def show
+    @calender = Calender.find(params[:id])
+  end 
+    
+  def new
+    @calender = Calender.new
+  end 
+  
+  def create 
+    Calender.create(calender_params)
+    redirect_to calender_path 
+  end 
+  
+  def edit 
+    @calender = Calender.find(params[:id])
+  end 
+  
+  def update
+    @calender = Calender.find(params[:id])
+    if @calender.update(calender_parameter)
+      redirect_to calenders_path, notice: "編集しました"
+    else 
+      render 'edit'
+    end 
+  end 
+    
+  def destroy
+    @calender = Calender.find(params[:id])
+    @calender.destroy 
+    redirect_to calenders_path, notice:"削除しました"
+  end 
+  
+  private 
+  
+  def calender_parameter 
+    params.require(:calender).permit(:title, :content, :start_time)
+  end 
+  
 end
