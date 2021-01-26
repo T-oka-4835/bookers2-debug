a9a08183a27f6a99f09979d072fd2a08c8565b5d
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-26 08:07:55 +0000

    [Add]検索機能のために、homes model を追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,10 @@ class HomesController < ApplicationController
     @books = Book.order("RANDOM()").limit(4)
     @ranks = Book.find(Favorite.group(:book_id).order('count(book_id) desc').limit(4).pluck(:book_id))
   end
+  
+  def search 
+    @book = Book.search(params[:search])
+  end 
 
   def about
   end
